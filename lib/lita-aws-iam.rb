37036ef560663b166fd41ae6607ccbf1df9acad1
Stefan - Zipkid - Goethals <stefan@zipkid.eu>
2019-12-11 10:28:08 +0100

    No Pry

--- lib/lita-aws-iam.rb
+++ lib/lita-aws-iam.rb
@@ -6,7 +6,7 @@ Lita.load_locales Dir[File.expand_path(
 
 require 'lita/handlers/awshelpers'
 require 'lita/handlers/aws_iam'
-require 'lita/handlers/lita_helper'
+# require 'lita/handlers/lita_helper'
 
 Lita::Handlers::AwsIam.template_root File.expand_path(
   File.join('..', '..', 'templates'),
